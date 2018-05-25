b9c7096a915a5ff6571dfed0b14e756e755813b8
udobnaja <udobnaja@gmail.com>
2018-05-25 17:20:58 +0300

    вход чмсла в последовательность

--- primes/index.js
+++ primes/index.js
@@ -12,7 +12,7 @@ const getPrimes = (num) => (num > 0) ? Array
 
 // O(n½),
 
-console.log(getPrimes(7)); // [1, 2, 3, 5, 7]
+console.log(getPrimes(8)); // [1, 2, 3, 5, 7]
 console.log(getPrimes(0)); //  []
 console.log(getPrimes(-1)); //  []
 
@@ -25,9 +25,9 @@ const eratosthenesSieve = (num) => {
         res[i] = true;
     }
 
-    for (let i = 2; i*i < num; i++) {
+    for (let i = 2; i*i <= num; i++) {
         if (res[i] === true) {
-            for (let k = i*i; k < num; k +=i ) {
+            for (let k = i*i; k <= num; k +=i ) {
                 res[k] = false;
             }
         }
@@ -42,4 +42,6 @@ const eratosthenesSieve = (num) => {
 };
 //O(n log(log n))}
 
-console.log(eratosthenesSieve(7));
+console.log(eratosthenesSieve(8));
+console.log(eratosthenesSieve(0));
+console.log(eratosthenesSieve(-1));
