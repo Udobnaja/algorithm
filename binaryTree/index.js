a4c077ff4d77c34c1cc8e9ca68adeb3531467e35
udobnaja <udobnaja@gmail.com>
2018-05-28 01:33:32 +0300

    DFS к binary_tree

--- binaryTree/index.js
+++ binaryTree/index.js
@@ -31,6 +31,30 @@ class BinaryTree {
         }
     }
 
+    DFS(value) {
+        const stack = [];
+
+        stack.push(this.root);
+
+        while (stack.length > 0) {
+            const node = stack.pop();
+
+            if (node.value === value) {
+                return node;
+            }
+
+            if (node.left !== null) {
+                stack.push(node.left);
+            }
+
+            if (node.right !== null) {
+                stack.push(node.right);
+            }
+        }
+
+        return null;
+    }
+
     createNode(value) {
         return {
             value,
@@ -49,4 +73,7 @@ binaryTree.addNode(7);
 binaryTree.addNode(12);
 
 console.log(binaryTree);
+
+console.log(binaryTree.DFS(7));
+console.log(binaryTree.DFS(11));
 
