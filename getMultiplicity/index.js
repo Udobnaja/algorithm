9fd697cf116b3845388ab1dcb0fcf23a63f254ea
udobnaja <udobnaja@gmail.com>
2018-05-29 13:56:13 +0300

    на массивах это лучуше работает

--- getMultiplicity/index.js
+++ getMultiplicity/index.js
@@ -10,19 +10,43 @@ const compare = (a, b) =>  {
     return 0;
 };
 
-const getMultiplicity = (arr) => arr.sort(compare).reduce((acc, el) => {
+// const getMultiplicity = (arr) => arr.sort(compare).reduce((acc, el) => {
+//
+//     if (el === +acc.slice(-1) + 1) {
+//         let index = acc.length;
+//         if (acc[acc.length - 2] === '-') {
+//             index = acc.length - 2;
+//         }
+//         acc = acc.substr(0, index) + '-' + el;
+//     } else if (el > +acc.slice(-1) + 1){
+//         acc = acc + ',' + el;
+//     }
+//
+//     return acc;
+// }, String(arr[0]));
 
-    if (el === +acc.slice(-1) + 1) {
-        let index = acc.length;
-        if (acc[acc.length - 2] === '-') {
-            index = acc.length - 2;
-        }
-        acc = acc.substr(0, index) + '-' + el;
-    } else if (el > +acc.slice(-1) + 1){
-        acc = acc + ',' + el;
+const getMultiplicity = (arr) => arr.sort(compare).reduce((acc, el, index, yArr) => {
+
+    const nextElem = arr[index + 1];
+    const prevElem = arr[index - 1];
+
+    if (nextElem - 1 !== el) {
+
+        acc.push(el);
+
+       if (index !== arr.length - 1){
+           acc.push(',');
+       }
+
+
+    } else if (el - 1 !== prevElem) {
+        acc.push(el);
+        acc.push('-');
     }
 
     return acc;
-}, String(arr[0]));
+}, []).join('');
 
-console.log(getMultiplicity([1, 4, 5, 2, 3, 9, 8, 11, 0]));
+// console.log(getMultiplicity([1, 4, 5, 2, 3, 9, 8, 11, 0]));
+
+console.log(getMultiplicity([1, 4, 5, 2, 3, 9, 8, 11, 0, 101, 15, 16, 17]));
