bade1459e0f3c197335eed298246a053ea2764fd
Anna Udobnaja <Udobnaja@users.noreply.github.com>
2018-06-13 14:23:16 +0200

    fix compare

--- getMultiplicity/index.js
+++ getMultiplicity/index.js
@@ -1,31 +1,6 @@
-const compare = (a, b) =>  {
-    if (a < b) {
-        return -1;
-    }
+const compare = (a, b) =>  a - b;
 
-    if (a > b) {
-        return 1
-    }
-
-    return 0;
-};
-
-// const getMultiplicity = (arr) => arr.sort(compare).reduce((acc, el) => {
-//
-//     if (el === +acc.slice(-1) + 1) {
-//         let index = acc.length;
-//         if (acc[acc.length - 2] === '-') {
-//             index = acc.length - 2;
-//         }
-//         acc = acc.substr(0, index) + '-' + el;
-//     } else if (el > +acc.slice(-1) + 1){
-//         acc = acc + ',' + el;
-//     }
-//
-//     return acc;
-// }, String(arr[0]));
-
-const getMultiplicity = (arr) => arr.sort(compare).reduce((acc, el, index, yArr) => {
+const getMultiplicity = (arr) => arr.sort(compare).reduce((acc, el, index) => {
 
     const nextElem = arr[index + 1];
     const prevElem = arr[index - 1];
@@ -47,6 +22,21 @@ const getMultiplicity = (arr) => arr.sort(compare).reduce((acc, el, index, yArr)
     return acc;
 }, []).join('');
 
-// console.log(getMultiplicity([1, 4, 5, 2, 3, 9, 8, 11, 0]));
+console.log(getMultiplicity([1, 4, 5, 2, 3, 9, 8, 11, 0]));
 
 console.log(getMultiplicity([1, 4, 5, 2, 3, 9, 8, 11, 0, 101, 15, 16, 17]));
+
+// const getMultiplicity = (arr) => arr.sort(compare).reduce((acc, el) => {
+//
+//     if (el === +acc.slice(-1) + 1) {
+//         let index = acc.length;
+//         if (acc[acc.length - 2] === '-') {
+//             index = acc.length - 2;
+//         }
+//         acc = acc.substr(0, index) + '-' + el;
+//     } else if (el > +acc.slice(-1) + 1){
+//         acc = acc + ',' + el;
+//     }
+//
+//     return acc;
+// }, String(arr[0]));
